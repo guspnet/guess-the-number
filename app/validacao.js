16bf4d495e40d7360f8a7873638a677f3ebb9aee
guspnet <guspnet@gmail.com>
2022-12-07 15:00:36 -0300

    Aula 04 do projeto do jogo de adivinhar um número da Alura

---
+++ app/validacao.js
@@ -0,0 +1,41 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    chute = chute.replaceAll(' ', '')
+    const numero = +chute //Ao colocar o sinal de soma ele já tenta converter para número automaticamente.
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML= `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    }else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-wide-short"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-wide-short"></i></div>
+        `
+    }
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
